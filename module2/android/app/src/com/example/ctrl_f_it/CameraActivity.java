14e2e533025fe129a8ee57994dfe5167e324641a
Brendan <brendan__lau@hotmail.com>
2014-03-11 12:05:31 -0700

    Basic camera function working still some runtime bugs

--- module2/android/app/src/com/example/ctrl_f_it/CameraActivity.java
+++ module2/android/app/src/com/example/ctrl_f_it/CameraActivity.java
@@ -1,34 +1,48 @@
 package com.example.ctrl_f_it;
 
-import android.annotation.SuppressLint;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 import android.app.Activity;
 import android.hardware.Camera;
-import android.os.Build;
+import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class CameraActivity extends Activity {
 
-	@SuppressLint("NewApi")
+	private Camera mCamera;
+    private CameraPreview mPreview;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		int PORTRAIT = 90;
-		int camId = 0;
-		Camera cam;
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_camera);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+        mCamera.setDisplayOrientation(90);
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
 		
-		// Make sure we're running on Honeycomb or higher to use ActionBar APIs
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            // Show the Up button in the action bar.
-            getActionBar().setDisplayHomeAsUpEnabled(true);
-        }
-
-        cam = Camera.open(camId);
-        Camera.Parameters param = cam.getParameters();
-        cam.setParameters(param);
-        cam.setDisplayOrientation(PORTRAIT);
+        Button captureButton = (Button) findViewById(R.id.button_capture);
+        captureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCamera.takePicture(null, null, mPicture);
+            }
+        });
 	}
 
 	@Override
@@ -37,4 +51,56 @@ public class CameraActivity extends Activity {
 		getMenuInflater().inflate(R.menu.camera, menu);
 		return true;
 	}
+	
+	/** A safe way to get an instance of the Camera object. */
+	public static Camera getCameraInstance(){
+	    Camera c = null;
+	    try {
+	        c = Camera.open(); // attempt to get a Camera instance
+	    }
+	    catch (Exception e){
+	        // Camera is not available (in use or does not exist)
+	    }
+	    return c; // returns null if camera is unavailable
+	}
+	
+	private PictureCallback mPicture = new PictureCallback() {
+
+	    @Override
+	    public void onPictureTaken(byte[] data, Camera camera) {
+
+	        File pictureFile = getOutputMediaFile();
+	        if (pictureFile == null){
+	            return;
+	        }
+
+	        try {
+	            FileOutputStream fos = new FileOutputStream(pictureFile);
+	            fos.write(data);
+	            fos.close();
+	        } catch (FileNotFoundException e) {
+	            Log.d("PictureCallback", "File not found: " + e.getMessage());
+	        } catch (IOException e) {
+	            Log.d("PictureCallback", "Error accessing file: " + e.getMessage());
+	        }
+	    }
+	};
+	
+	private static File getOutputMediaFile() {
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                "Ctrl_F_It");
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("Ctrl_F_It", "failed to create directory");
+                return null;
+            }
+        }
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+
+        return mediaFile;
+    }
 }
+
