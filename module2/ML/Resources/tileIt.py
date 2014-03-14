85e43b8df065b30475a7717a2e06ff5794f8f3a2
Wilson Y. Wong <Wilson.Young.Wong@gmail.com>
2014-03-14 00:05:02 -0700

    tile the fonts

--- module2/ML/Resources/tileIt.py
+++ module2/ML/Resources/tileIt.py
@@ -2,17 +2,18 @@ import Image
 import string
 import glob
 
-images = list(glob.glob('fonts/*.bmp'))
-white = (255, 255, 255, 255)
+images = list(glob.glob('Training_Fonts/*.bmp'))
+white = 255
 
 let = list(string.ascii_lowercase)
 
 fonts = 0
 
 while images:
-  im = Image.open(images.pop(0))
+  im = Image.open(images.pop(0)).convert('L')
 
   pixels = list(im.getdata())
+  pixels = [0 if x == 0 else white for x in pixels]
   width, height = im.size
   pixels = [pixels[i * width:(i + 1) * width] for i in xrange(height)]
 
@@ -21,7 +22,7 @@ while images:
 
   for y in range(0, width):
     vertLine = False
-    for x in range(0, height):
+    for x in range(1, height-1):
       if(pixels[x][y] == white):
         vertLine = False
         break
@@ -30,7 +31,7 @@ while images:
     if(vertLine):
       bars.append(y)
 
-  print bars
+#  print bars
 
   leftx = 0
   for l in let:
@@ -38,11 +39,11 @@ while images:
       rightx = width
     else:
       rightx = bars.pop(0)
-    print (leftx, 0, rightx, height)
-    im.crop((leftx, 0, rightx, height)).save('tiles/{0}{1}.bmp'.format(l, fonts))
+#    print (leftx, 0, rightx, height)
+    im.crop((leftx, 1, rightx, height-1)).save('tiles/{0}{1}.bmp'.format(l, fonts))
     leftx = rightx + 1
 
-  im.close()
+  fonts += 1
 
 
 
