2edcb5e0ac4bfa68c36d8a099c13d4f0a2b8811c
aozo <dan_jin794@hotmail.com>
2014-03-16 23:08:41 -0700

    Able to read csv input

--- module2/android/app/src/com/example/ctrl_f_it/ProcessingActivity.java
+++ module2/android/app/src/com/example/ctrl_f_it/ProcessingActivity.java
@@ -9,6 +9,7 @@ import org.ejml.simple.SimpleMatrix;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 
 
@@ -18,6 +19,7 @@ public class ProcessingActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_processing);
+		charRecognition();
 	}
 
 	@Override
@@ -27,30 +29,32 @@ public class ProcessingActivity extends Activity {
 		return true;
 	}
 
+	public static final int INPUT_WIDTH = 20;
+	public static final int INPUT = 400;
 	public static final int OUTPUT = 26;
-	public static final int HIDDEN_UNITS = 85;
-	// 32x32 size character image
-	public static final int INPUT = 1024;
+	public static final int HIDDEN_UNITS = 72;
 
 	public void charRecognition() {
 		double[][] theta1 = parseCSV("sdcard/Ctrl_F_It/theta1.csv", HIDDEN_UNITS, INPUT + 1);
 		double[][] theta2 = parseCSV("sdcard/Ctrl_F_It/theta2.csv", OUTPUT, HIDDEN_UNITS + 1);
-
+		double[][] input = inputUnroll(parseCSV("sdcard/Ctrl_F_It/a.csv", INPUT_WIDTH, INPUT_WIDTH));
+		int matchedChar = forwardPropagation(theta1, theta2, input);
+		Log.d("Testing", Integer.toString(matchedChar));
 	}
-	
+
 	/* forwardPropagation
 	 * Applies feed forward propagation within the neural network
 	 * Parameters:
-	 * 		double[][] theta1: Each row contains the theta values for one hidden unit and the bias (INPUT + 1 columns)
-	 * 		double[][] theta2: Each row contains the theta values for each output character and the bias
-	 * 		double[] input: Unrolled vector of INPUT elements, each representing one pixel of the image
+	 * 		double[][] theta1: 2D array containing the theta values for the hidden layer (size HIDDEN_UNITS x INPUT + 1)
+	 * 		double[][] theta2: 2D array containing the theta values for the hidden layer (size OUTPUT x HIDDEN_UNITS + 1)
+	 * 		double[] input: Unrolled vector of values each representing one pixel of the image (size INPUT x 1)
 	 * Return:
 	 * 		int: The ith character of the alphabet that has the highest calculated propability
 	 */
-	public int forwardPropagation(double[][] theta1, double[][] theta2, double[] input) {
+	public int forwardPropagation(double[][] theta1, double[][] theta2, double[][] input) {
 		SimpleMatrix theta1Array = new SimpleMatrix(theta1);
 		SimpleMatrix theta2Array = new SimpleMatrix(theta2);
-		SimpleMatrix inputArray = new SimpleMatrix(INPUT, 1);
+		SimpleMatrix inputArray = new SimpleMatrix(input);
 		SimpleMatrix inputArrayWithBias = new SimpleMatrix(INPUT + 1, 1);
 		SimpleMatrix h1Array;
 		SimpleMatrix h1ArrayWithBias = new SimpleMatrix(HIDDEN_UNITS + 1, 1);
@@ -87,7 +91,7 @@ public class ProcessingActivity extends Activity {
 
 		return bestMatch + 1;
 	}
-	
+
 	/* parseCSV
 	 * Parses a csv file and stores the contents into a 2d double array
 	 * Parameters:
@@ -98,7 +102,7 @@ public class ProcessingActivity extends Activity {
 	 * 		double[][]: Array containing the contents of the csv file
 	 */
 	public double[][] parseCSV(String inFile, int rows, int columns) {
-		double[][] theta = new double[rows][columns];
+		double[][] parsedValues = new double[rows][columns];
 		BufferedReader br = null;
 		String row;
 		String[] separatedRow;
@@ -111,7 +115,7 @@ public class ProcessingActivity extends Activity {
 				row = br.readLine();
 				separatedRow = row.split(cvsDelimiter);
 				for(j = 0; j < columns; j++) {
-					theta[i][j] = Double.parseDouble(separatedRow[j]);
+					parsedValues[i][j] = Double.parseDouble(separatedRow[j]);
 				}
 			}
 		} catch (FileNotFoundException e) {
@@ -127,6 +131,28 @@ public class ProcessingActivity extends Activity {
 				}
 			}
 		}
-		return theta;
+		return parsedValues;
+	}
+
+	/* inputUnroll
+	 * Unrolls the 2D input matrix into a single column vector
+	 * Parameters:
+	 * 		double[][] input: 2D array of pixel values from the input (size root(INPUT) x root(INPUT))
+	 * 		double inputWidth: The length of the input array
+	 * Return:
+	 * 		double[][]: Unrolled column vector of the input (size INPUT x 1)
+	 */
+	public double[][] inputUnroll(double[][] input) {
+		double[][] unrolledInput = new double[INPUT][1];
+		int i,j;
+		int k = 0;
+
+		for(i = 0; i < INPUT_WIDTH; i++) {
+			for(j = 0; j < INPUT_WIDTH; j++) {
+				unrolledInput[k][0] = input[i][j];
+				k++;
+			}
+		}
+		return unrolledInput;
 	}
 }
