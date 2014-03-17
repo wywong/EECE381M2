af10c8185b012f4b507f090fb42aaf9ecf151e9a
Wilson Y. Wong <Wilson.Young.Wong@gmail.com>
2014-03-16 23:27:37 -0700

    testing photos now slightly more efficient

--- module2/ML/Resources/nnTrain/check.m
+++ module2/ML/Resources/nnTrain/check.m
@@ -1,5 +1,14 @@
 load('theta.mat');
-X = imread('Selection_001.bmp');
-X = reshape(X, 1, prod(size(X)));
-X = double(X)/255;
-predict(Theta1, Theta2, X)
+
+letters = ['A' 'B' 'C' 'D' 'E' 'F' 'G' 'H' 'I' 'J' 'K' 'L' 'M' 'N' 'O'
+          'P' 'Q' 'R' 'S' 'T' 'U' 'V' 'W' 'X' 'Y' 'Z']';
+
+imageFiles = dir('photo/*.bmp');
+
+for ii = imageFiles'
+  X = imread(strcat('photo/', ii.name));
+  X = reshape(X, 1, prod(size(X)));
+  bSize = sizeof(X(1,1));
+  X = double(X) / (2^(8*bSize) - 1);
+  fprintf('%s maps to %s\n', ii.name, letters(predict(Theta1, Theta2, X)))
+end
