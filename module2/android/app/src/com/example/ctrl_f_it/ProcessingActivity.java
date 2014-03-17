b78c35a5156d0efe6dd4888626844093b6d35ded
aozo <dan_jin794@hotmail.com>
2014-03-16 20:49:56 -0700

    Changed to ejml library

--- module2/android/app/src/com/example/ctrl_f_it/ProcessingActivity.java
+++ module2/android/app/src/com/example/ctrl_f_it/ProcessingActivity.java
@@ -5,12 +5,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-import org.apache.commons.math3.linear.Array2DRowRealMatrix;
-import org.apache.commons.math3.linear.RealMatrix;
+import org.ejml.simple.SimpleMatrix;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+
 
 public class ProcessingActivity extends Activity {
 
@@ -33,11 +33,11 @@ public class ProcessingActivity extends Activity {
 	public static final int INPUT = 1024;
 
 	public void charRecognition() {
-		double[][] theta1 = parseTheta("sdcard/Ctrl_F_It/theta1.csv", HIDDEN_UNITS, INPUT + 1);
-		double[][] theta2 = parseTheta("sdcard/Ctrl_F_It/theta2.csv", OUTPUT, HIDDEN_UNITS + 1);
+		double[][] theta1 = parseCSV("sdcard/Ctrl_F_It/theta1.csv", HIDDEN_UNITS, INPUT + 1);
+		double[][] theta2 = parseCSV("sdcard/Ctrl_F_It/theta2.csv", OUTPUT, HIDDEN_UNITS + 1);
 
 	}
-
+	
 	/* forwardPropagation
 	 * Applies feed forward propagation within the neural network
 	 * Parameters:
@@ -45,51 +45,59 @@ public class ProcessingActivity extends Activity {
 	 * 		double[][] theta2: Each row contains the theta values for each output character and the bias
 	 * 		double[] input: Unrolled vector of INPUT elements, each representing one pixel of the image
 	 * Return:
-	 * 		int: The (i + 1)th character of the alphabet that has the highest calculated propability
+	 * 		int: The ith character of the alphabet that has the highest calculated propability
 	 */
 	public int forwardPropagation(double[][] theta1, double[][] theta2, double[] input) {
-		RealMatrix theta1Array = new Array2DRowRealMatrix(theta1);
-		RealMatrix theta2Array = new Array2DRowRealMatrix(theta2);
-		RealMatrix inputArray = new Array2DRowRealMatrix(INPUT + 1, 1);
-		RealMatrix h1Array;
-		//Create a column vector with # of hidden units + 1 for the bias element
-		RealMatrix h1ArrayWithBias = new Array2DRowRealMatrix(HIDDEN_UNITS + 1, 1);
-		RealMatrix outputArray;
+		SimpleMatrix theta1Array = new SimpleMatrix(theta1);
+		SimpleMatrix theta2Array = new SimpleMatrix(theta2);
+		SimpleMatrix inputArray = new SimpleMatrix(INPUT, 1);
+		SimpleMatrix inputArrayWithBias = new SimpleMatrix(INPUT + 1, 1);
+		SimpleMatrix h1Array;
+		SimpleMatrix h1ArrayWithBias = new SimpleMatrix(HIDDEN_UNITS + 1, 1);
+		SimpleMatrix outputArray;
 		int i;
 		double sigmoidVal;
 		int bestMatch = 0;
 
 		//Initialize input array to include the bias value
-		inputArray.setEntry(0, 0, 1);
-		for(i = 1; i < INPUT + 1; i++) {
-			inputArray.setEntry(i, 0, input[i - 1]);
-		}
+		inputArrayWithBias.set(0, 0, 1);
+		inputArrayWithBias.insertIntoThis(1, 0, inputArray);
 
 		//sigmoid(theta1 * input)
-		h1Array = theta1Array.multiply(inputArray);
-		h1ArrayWithBias.setEntry(0, 0, 1);
-		for(i = 1; i < HIDDEN_UNITS + 1; i++) {
-			sigmoidVal = 1 / (1 + Math.exp(-h1Array.getEntry(i - 1, 0)));
-			h1ArrayWithBias.setEntry(i, 0, sigmoidVal);
+		h1Array = theta1Array.mult(inputArrayWithBias);
+		h1ArrayWithBias.set(0, 0, 1);
+		for(i = 0; i < HIDDEN_UNITS; i++) {
+			sigmoidVal = 1 / (1 + Math.exp(-h1Array.get(i, 0)));
+			h1Array.set(i, 0, sigmoidVal);
 		}
+		h1ArrayWithBias.insertIntoThis(1, 0, h1Array);
 
 		//sigmoid(theta2 * h1)
-		outputArray = theta2Array.multiply(h1ArrayWithBias);
+		outputArray = theta2Array.mult(h1ArrayWithBias);
 		for(i = 0; i < OUTPUT; i++) {
-			sigmoidVal = 1 / (1 + Math.exp(-outputArray.getEntry(i, 0)));
-			outputArray.setEntry(1, 0, sigmoidVal);
+			sigmoidVal = 1 / (1 + Math.exp(-outputArray.get(i, 0)));
+			outputArray.set(1, 0, sigmoidVal);
 		}
 
 		for(i = 0; i < OUTPUT; i++) {
-			if(outputArray.getEntry(i, 0) > bestMatch) {
+			if(outputArray.get(i, 0) > bestMatch) {
 				bestMatch = i;
 			}
 		}
 
-		return bestMatch;
+		return bestMatch + 1;
 	}
-
-	public double[][] parseTheta(String inFile, int rows, int columns) {
+	
+	/* parseCSV
+	 * Parses a csv file and stores the contents into a 2d double array
+	 * Parameters:
+	 * 		String inFile: Location of the csv file relative to the root directory of the android device
+	 * 		int rows: Number of rows to be parsed
+	 * 		int columns: Number of columns to be parsed
+	 * Return:
+	 * 		double[][]: Array containing the contents of the csv file
+	 */
+	public double[][] parseCSV(String inFile, int rows, int columns) {
 		double[][] theta = new double[rows][columns];
 		BufferedReader br = null;
 		String row;
