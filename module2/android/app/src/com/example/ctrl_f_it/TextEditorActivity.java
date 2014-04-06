d399ea1c25b953dab5dc6e92b79c0a09b28fb4fa
Brendan <brendan__lau@hotmail.com>
2014-04-06 13:56:34 -0700

    working text editor, with lifecycle changes

---
+++ module2/android/app/src/com/example/ctrl_f_it/TextEditorActivity.java
@@ -0,0 +1,99 @@
+package com.example.ctrl_f_it;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class TextEditorActivity extends Activity {
+
+    private EditText mTitleText;
+    private EditText mBodyText;
+    private Long mRowId;
+    private NotesDbAdapter mDbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        mDbHelper = new NotesDbAdapter(this);
+        mDbHelper.open();
+        
+        setContentView(R.layout.text_edit);
+
+        mTitleText = (EditText) findViewById(R.id.title);
+        mBodyText = (EditText) findViewById(R.id.body);
+
+        Button confirmButton = (Button) findViewById(R.id.confirm);
+        Button searchButton = (Button) findViewById(R.id.search);
+        
+        if(savedInstanceState == null)
+        	mRowId =  null;
+        else
+            mRowId = (Long) savedInstanceState.getSerializable(NotesDbAdapter.KEY_ROWID);
+        
+        if (mRowId == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras != null)
+            	mRowId = extras.getLong(NotesDbAdapter.KEY_ROWID);
+            else
+                mRowId = null;
+        }
+
+        populateFields();
+        
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                setResult(RESULT_OK);
+                finish();
+            }
+
+        });
+    }
+    
+    @SuppressWarnings("deprecation")
+	private void populateFields() {
+    	if(mRowId != null) {
+    		Cursor note = mDbHelper.fetchNote(mRowId);
+    		startManagingCursor(note);
+    		mTitleText.setText(note.getString(note.getColumnIndexOrThrow(NotesDbAdapter.KEY_TITLE)));
+    		mBodyText.setText(note.getString(note.getColumnIndexOrThrow(NotesDbAdapter.KEY_BODY)));
+    	}
+    }
+    
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        saveState();
+        outState.putSerializable(NotesDbAdapter.KEY_ROWID, mRowId);
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveState();
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        populateFields();
+    }
+    
+    private void saveState() {
+        String title = mTitleText.getText().toString();
+        String body = mBodyText.getText().toString();
+
+        if (mRowId == null) {
+            long id = mDbHelper.createNote(title, body);
+            if (id > 0) {
+                mRowId = id;
+            }
+        } else {
+            mDbHelper.updateNote(mRowId, title, body);
+        }
+    }
+}
