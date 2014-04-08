fe5c612c5e3674201bfa6004fa575b6f17f8630c
Wilson Y. Wong <Wilson.Young.Wong@gmail.com>
2014-04-08 01:39:40 -0700

    untested push pull

--- module2/software/src/nios_software.c
+++ module2/software/src/nios_software.c
@@ -5,6 +5,12 @@
 #include <system.h>
 #include "../inc/string_graphics.h"
 
+#define PUSH 0	// 0 push
+#define PULL 1	// 1 pull
+
+#define TRUE 1
+#define FALSE 0
+
 int main()
 {
 	int i;
@@ -12,6 +18,7 @@ int main()
 	unsigned char parity;
 	unsigned char message[] = "EECE381 is so much fun";
 	unsigned char* buffer;
+	int bufSize = 0;
 	initBuffers();
 
 	//INITIALIZATION
@@ -24,56 +31,92 @@ int main()
 		alt_up_rs232_read_data(uart, &data, &parity);
 	}
 
-	// WAIT FOR MESSAGE TO COME FROM MIDDLEMAN (SENT FROM ANDROID)
+	while(TRUE) {
+		// WAIT FOR MESSAGE TO COME FROM MIDDLEMAN (SENT FROM ANDROID)
+		printf("Waiting for data to come from the Middleman\n");
 
-	printf("Waiting for data to come from the Middleman\n");
-
-	while (alt_up_rs232_get_used_space_in_read_FIFO(uart) == 0) ;
-
-	// First byte is the number of characters in our message
-	alt_up_rs232_read_data(uart, &data, &parity);
-	int num_to_receive = (int)data;
-
-	//create a buffer the same as the size of the message sent from android
-	buffer = (unsigned char*) malloc(num_to_receive * sizeof(unsigned char));
-
-	printf("About to receive %d characters:\n", num_to_receive);
-
-	for (i = 0; i < num_to_receive; i++) {
 		while (alt_up_rs232_get_used_space_in_read_FIFO(uart) == 0) ;
 
+		// First byte is the number of characters in our message
 		alt_up_rs232_read_data(uart, &data, &parity);
+		int num_to_receive = (int)data;
 
-		buffer[i] = (char) data;
-	}
-	buffer[i] = '\0';
-
-	//write string to vga display
-	writeString(buffer, 0, 0);
-	printf("\n");
-
-	//BEGIN SENDING MESSAGE FROM DE2 TO ANDROID (RETURNS ORIGINAL MESSAGE)
-	printf("Clearing read buffer to start\n");
-
-	while (alt_up_rs232_get_used_space_in_read_FIFO(uart)) {
+		// action code
+		// 0 push
+		// 1 pull
 		alt_up_rs232_read_data(uart, &data, &parity);
+		int actionCode = (int) data;
+
+		if(actionCode == PULL) {
+			if(bufSize == 0) {
+				char* empty = "0";
+				//BEGIN SENDING MESSAGE FROM DE2 TO ANDROID (RETURNS ORIGINAL MESSAGE)
+				printf("Clearing read buffer to start\n");
+
+				while (alt_up_rs232_get_used_space_in_read_FIFO(uart)) {
+					alt_up_rs232_read_data(uart, &data, &parity);
+				}
+
+				printf("Sending the message to the Middleman\n");
+
+				// Start with the number of bytes in our message
+
+				alt_up_rs232_write_data(uart, (unsigned char) strlen(buffer));
+
+				// Now send the actual message to the Middleman
+				for (i = 0; i < strlen(empty); i++) {
+						alt_up_rs232_write_data(uart, empty[i]);
+				}
+			} else {
+				//BEGIN SENDING MESSAGE FROM DE2 TO ANDROID (RETURNS ORIGINAL MESSAGE)
+				printf("Clearing read buffer to start\n");
+
+				while (alt_up_rs232_get_used_space_in_read_FIFO(uart)) {
+					alt_up_rs232_read_data(uart, &data, &parity);
+				}
+
+				printf("Sending the message to the Middleman\n");
+
+				// Start with the number of bytes in our message
+
+				alt_up_rs232_write_data(uart, (unsigned char) strlen(buffer));
+
+				// Now send the actual message to the Middleman
+
+				for (i = 0; i < strlen(buffer); i++) {
+						alt_up_rs232_write_data(uart, buffer[i]);
+				}
+
+				printf("Message Echo Complete\n");
+			}
+		} else {
+			if(bufSize != 0) {
+				free(buffer);
+			}
+			//create a buffer the same as the size of the message sent from android
+			buffer = (unsigned char*) malloc(num_to_receive * sizeof(unsigned char));
+
+			bufSize = num_to_receive;
+
+			printf("About to receive %d characters:\n", num_to_receive);
+
+			for (i = 0; i < num_to_receive; i++) {
+				while (alt_up_rs232_get_used_space_in_read_FIFO(uart) == 0) ;
+
+				alt_up_rs232_read_data(uart, &data, &parity);
+
+				buffer[i] = (char) data;
+			}
+			buffer[i] = '\0';
+
+			//write string to vga display
+			writeString(buffer, 0, 0);
+			printf("\n");
+		}
+
 	}
 
-	printf("Sending the message to the Middleman\n");
 
-	// Start with the number of bytes in our message
-
-	alt_up_rs232_write_data(uart, (unsigned char) strlen(buffer));
-
-	// Now send the actual message to the Middleman
-
-	for (i = 0; i < strlen(buffer); i++) {
-			alt_up_rs232_write_data(uart, buffer[i]);
-	}
-
-	printf("Message Echo Complete\n");
-
-	free(buffer);
 
 	return 0;
 }
